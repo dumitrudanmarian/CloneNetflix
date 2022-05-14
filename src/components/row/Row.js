ed95afc8d57953529b59958c8612c97c22e17122
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-05-15 00:59:25 +0300

    Movie info on mouse hover

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import instance from '../../api/axios';
 import "./Row.css";
+import { FaPlay, FaThumbsUp, FaThumbsDown, FaPlus } from "react-icons/fa";
+
 
 const basePosterUrl = "https://image.tmdb.org/t/p/original/"
 
@@ -16,20 +18,34 @@ const Row = ({ title, fetchUrl, isLargeImage }) => {
         fetchData();
     }, [fetchUrl]);
 
-    // console.table(movies)
+    console.table(movies)
 
     return (
         <div className='row'>
             <h2>{title}</h2>
             <div className="row-posters">
+
                 {movies.map(movie => (
-                    <img
-                        key={movie.id}
-                        className={`row-poster 
-                                    ${isLargeImage && "row-large-poster"} 
-                                    ${(!movie.poster_path || !movie.backdrop_path) && "row-hidden-poster"}`}
-                        src={`${basePosterUrl}${isLargeImage ? movie.poster_path : movie.backdrop_path}`}
-                        alt={movie.name} />
+                    <div key={movie.id} className='each-movie'>
+                        <img
+
+                            className={`row-poster 
+                        ${(!movie.poster_path || !movie.backdrop_path) && "row-hidden-poster"}`}
+                            src={`${basePosterUrl}${movie.backdrop_path}`}
+                            alt={movie.name} />
+
+                        <div className="poster-info">
+                            <div className="icons">
+                                <FaPlay />
+                                <FaPlus />
+                                <FaThumbsUp />
+                                <FaThumbsDown />
+                            </div>
+                            <h2>{movie.title || movie.name}</h2>
+                            <p>{movie.overview}</p>
+
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
